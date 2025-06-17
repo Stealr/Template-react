3ffadd34520f4cde09d028ee80b0358891ef2401
Stealr <baranovm003@yandex.ru>
2025-06-17 12:32:37 +0300

    убран try catch

--- src/react/services/api/testApi/test.js
+++ src/react/services/api/testApi/test.js
@@ -3,12 +3,7 @@ import apiClient from '@services/apiClient';
 import { ENDPOINTS } from '@services/apiConfig';
 
 export async function fetchTestData() {
-  try {
     const response = await apiClient.get(ENDPOINTS.test.getTest);
     // console.log(response);
     return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 }
